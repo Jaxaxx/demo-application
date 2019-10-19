50ae4fc3b082997ae3bcc4bb62acb73db1430cda
liming <liming@wenxiangcn.com>
2019-10-19 17:41:22 +0800

    Update HashMapTest.java
    .

--- src/main/java/com/mine/demo/map/HashMapTest.java
+++ src/main/java/com/mine/demo/map/HashMapTest.java
@@ -2,14 +2,23 @@ package com.mine.demo.map;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
-/**
- * 存入顺序和取出顺序不一致：无序
- */
 public class HashMapTest {
 
     public static void main(String[] args) {
+//        test1();
+        test2();
 
+        int hash = hash(1);
+        System.out.println(hash);
+
+    }
+
+    /**
+     * 存入顺序和取出顺序不一致：无序
+     */
+    static void test1() {
         Map<Integer, Integer> map = new HashMap<>();
 
         map.put(11, 1);
@@ -25,4 +34,22 @@ public class HashMapTest {
             System.out.println(k + " : " + v);
         });
     }
+
+    static void test2() {
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put("name", "john");
+
+        int index = Math.abs("name".hashCode()) % 16;
+
+        System.out.println("name.hashCode:---->" + "name".hashCode());
+        System.out.println("index:" + index);
+
+    }
+
+    static final int hash(Object key) {
+        int h;
+        return (key == null) ? 0 : (h = key.hashCode())  ^  (h >> 16);
+    }
+
+
 }
